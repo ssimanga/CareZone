a6ca80acec14c8f9b268ae036c8356837ec34c3c
Simanga Shabalala <simangae@gmail.com>
2020-11-09 13:30:23 +0200

    Initial Set Up

---
+++ CareZone/CareZone.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CareZone.WebUI.MvcApplication" Language="C#" %>
---
+++ CareZone/CareZone.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CareZone.WebUI.Startup))]
+namespace CareZone.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
